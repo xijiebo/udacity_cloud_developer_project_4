80bc3eb4eae0d1e2b480f92bc7c89a07e219f273
Jiebo Xi <xijiebo@gmail.com>
2021-12-23 16:30:17 -0600

    udpated update response

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -22,9 +22,7 @@ export const handler = middy(
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
-      body: JSON.stringify({ 
-        
-      })
+      body: ''
     }
   })
 
