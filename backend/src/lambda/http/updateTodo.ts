d6dc650364bf2cec512a0337f95362e9413145d3
Jiebo Xi <xijiebo@gmail.com>
2021-12-23 17:24:57 -0600

    udpate validation

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -5,15 +5,35 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../helpers/todos'
+import { updateTodo, getTodosPerTodoId } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
+
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
     const updateToDoRequest: UpdateTodoRequest = JSON.parse(event.body)
+
+    if(!updateToDoRequest.name || !updateToDoRequest.dueDate) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'name and dueDate are required.'
+        })
+      }     
+    }
+
+    const item = await getTodosPerTodoId(getUserId(event), event.pathParameters.todoId)
+    if(!item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'Todo item not found.'
+        })
+      };   
+    }
 
     await updateTodo(getUserId(event), event.pathParameters.todoId, updateToDoRequest)
     return {
